c153c4ae94e1229c66e736f05e6e555c9c7581e0
Matt Takemoto <matt.t.takemoto@gmail.com>
2017-08-29 16:21:01 -1000

    adding player deletion

--- score-keep/client/main.js
+++ score-keep/client/main.js
@@ -10,8 +10,13 @@ import {Players} from './../imports/api/players';
 
 const renderPlayers = (playersList) => {
   return playersList.map((player) => {
-    return <p key={player._id}>{player.name} has {player.score} point(s)</p>;
-  })
+    return (
+      <p key={player._id}>
+        {player.name} has {player.score} point(s).
+        <button onClick={() => Players.remove({_id: player._id})}>X</button>
+      </p>
+    );
+  });
 };
 
 const insertPlayer = function(playerName) {
