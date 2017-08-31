bab568025b687b2ddf8b4c9b9d701834f9c66115
Matt Takemoto <matt.t.takemoto@gmail.com>
2017-08-30 15:21:04 -1000

    Adding player component

--- score-keep/client/main.js
+++ score-keep/client/main.js
@@ -5,6 +5,7 @@ import {Tracker} from 'meteor/tracker';
 import {Players} from './../imports/api/players';
 import TitleBar from './../imports/ui/TitleBar';
 import AddPlayer from './../imports/ui/AddPlayer';
+import Player from './../imports/ui/Player';
 
 /* Notes
  * JSX should only return 1 root element
@@ -16,18 +17,7 @@ import AddPlayer from './../imports/ui/AddPlayer';
  */
 const renderPlayers = (playersList) => {
   return playersList.map((player) => {
-    return (
-      <p key={player._id}>
-        {player.name} has {player.score} point(s).
-        <button onClick={() => {
-          Players.update(player._id, {$inc: {score: 1}});
-        }}>+1</button>
-        <button onClick={() => {
-          Players.update(player._id, {$inc: {score: -1}});
-        }}>-1</button>
-        <button onClick={() => Players.remove(player._id)}>X</button>
-      </p>
-    );
+    return <Player key={player._id} player={player} />;
   });
 };
 
@@ -40,7 +30,6 @@ Meteor.startup(() => {
       <div>
         <TitleBar title={title} subtitle="Created by Matt"/>
         {renderPlayers(players)}
-
         <AddPlayer />
       </div>
     );
