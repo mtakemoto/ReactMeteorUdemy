e510000866aa757016e5ef052d62a53e7d94b264
Matt Takemoto <matt.t.takemoto@gmail.com>
2017-08-30 15:55:05 -1000

    Score sorting implemented

--- score-keep/client/main.js
+++ score-keep/client/main.js
@@ -9,11 +9,16 @@ import App from './../imports/ui/App';
 
 /* Notes
  * JSX should only return 1 root element
+ * Use Meteor docs for sorting
  */
 
 Meteor.startup(() => {
   Tracker.autorun(() => {
-    let players = Players.find().fetch();
+    let players = Players.find({}, {
+      sort: {
+        score: -1
+      }
+    }).fetch();
     let title = 'Score Keep';
     ReactDOM.render(<App title={title} players={players}/>, document.getElementById('app'));
   });
