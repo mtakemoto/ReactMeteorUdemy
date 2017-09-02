59fb8a639591717a0de544cfff15b90e8ed1c4f8
Matt Takemoto <matt.t.takemoto@gmail.com>
2017-09-01 22:22:47 -1000

    Added player place display

--- score-keep/imports/api/players.js
+++ score-keep/imports/api/players.js
@@ -1,3 +1,23 @@
 import {Mongo} from 'meteor/mongo';
+import numeral from 'numeral';
 
 export const Players = new Mongo.Collection('players');
+
+/* function calculatePlayerPositions
+ * pre: players array from MongoDB
+ * post: calculates place rank, accounting for ties
+ * returns: new array with formatted rank string
+ */
+export const calculatePlayerPositions = (players) => {
+  let rank = 1;
+  return players.map((player, index) => {
+    if(index !== 0 && players[index - 1].score > player.score) {
+      rank++;
+    }
+
+    return {
+      ...player,
+      position: numeral(rank).format('0o')
+    }
+  });
+};
--- score-keep/imports/ui/Player.js
+++ score-keep/imports/ui/Player.js
@@ -4,25 +4,26 @@ import PropTypes from 'prop-types';
 
 export default class Player extends React.Component {
   render() {
+    let p = this.props;
     return (
-       <div key={this.props.player._id} className="item">
+       <div key={p.player._id} className="item">
          <div className="player">
            <div>
-             <h3 className="player__name">{this.props.player.name}</h3>
+             <h3 className="player__name">{p.player.name}</h3>
              <p className="player__stats">
-                {this.props.player.score} point(s).
+                {p.player.rank} {p.player.position} {p.player.score} point(s).
              </p>
            </div>
 
            <div className="player__actions">
              <button className="button button--round" onClick={() => {
-               Players.update(this.props.player._id, {$inc: {score: 1}});
+               Players.update(p.player._id, {$inc: {score: 1}});
              }}>+1</button>
              <button className="button button--round" onClick={() => {
-               Players.update(this.props.player._id, {$inc: {score: -1}});
+               Players.update(p.player._id, {$inc: {score: -1}});
              }}>-1</button>
              <button className="button button--round" onClick={() => {
-               Players.remove(this.props.player._id);
+               Players.remove(p.player._id);
              }}>X</button>
            </div>
          </div>
