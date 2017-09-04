8d72c370e7326f4f7845e9683ef753b7fd461872
Matt Takemoto <matt.t.takemoto@gmail.com>
2017-09-04 11:51:11 -1000

    adding basic routes

--- short-lnk/client/main.js
+++ short-lnk/client/main.js
@@ -1,11 +1,27 @@
 import { Meteor } from 'meteor/meteor';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, browserHistory } from 'react-router';
 
 import Signup from '../imports/ui/Signup';
 import Link from '../imports/ui/Link';
+import NotFound from '../imports/ui/NotFound';
+import Login from '../imports/ui/Login';
 
+
+/* Routing
+ * 1 instance of Router
+ * many instances of Route
+ */
+const routes = (
+  <Router history={browserHistory}>
+    <Route path="/" component={Login}/>
+    <Route path="/signup" component={Signup}/>
+    <Route path="/links" component={Link}/>
+    <Route path="*" component={NotFound}/>
+  </Router>
+);
 
 Meteor.startup(() => {
-  ReactDOM.render(<Signup/>, document.getElementById('app'));
+  ReactDOM.render(routes, document.getElementById('app'));
 });
